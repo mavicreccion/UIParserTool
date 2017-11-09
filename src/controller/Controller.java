5defae5353062035c62c06c787265ed2dceda44b
mavicreccion <mavicreccion@users.noreply.github.com>
2017-11-09 22:02:12 +0800

    changed creating of java components in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -34,44 +34,6 @@ public class Controller {
 		
 		// 1. parse
 		ArrayList<ParsedElement> elements = model.JSONparse();
-		for (int i=0; i < elements.size(); i++)
-		{
-			Font f = new Font("ARIAL", Font.PLAIN, elements.get(i).getFontsize());
-			
-			switch(elements.get(i).getType())
-			{
-				case LABEL: 
-					JLabel label = new JLabel();
-					label.setText(elements.get(i).getText());
-					label.setLocation(elements.get(i).getX(), elements.get(i).getY());
-					label.setSize(elements.get(i).getWidth(),elements.get(i).getHeight());
-					label.setFont(f);
-					System.out.println("adding label");
-					view.addLabel(label);
-					break;
-				case BUTTON:
-					JButton button = new JButton();
-					button.setText(elements.get(i).getText());
-					button.setLocation(elements.get(i).getX(), elements.get(i).getY());
-					button.setSize(elements.get(i).getWidth(), elements.get(i).getHeight());
-					button.setFont(f);
-					button.setRolloverEnabled(false);
-					System.out.println("adding button");
-					view.addButton(button);
-					break;
-				case TEXTFIELD:
-					JTextField textfield = new JTextField();
-					textfield.setText(elements.get(i).getText());
-					textfield.setLocation(elements.get(i).getX(), elements.get(i).getY());
-					textfield.setSize(elements.get(i).getWidth(), elements.get(i).getHeight());
-					textfield.setFont(f);
-					System.out.println("adding textfield");
-					view.addTextBox(textfield);
-					break;
-			}
-			
-			
-		}
 		
 		/* 
 		 * 	2. for each parsed element, create a java component
@@ -79,6 +41,39 @@ public class Controller {
 		 *  -- set text when initializing the component
 		 *  -- call particular methods in the view when adding the component
 		*/		
+		for (int i=0; i < elements.size(); i++)
+		{
+			Font f = new Font("ARIAL", Font.PLAIN, elements.get(i).getFontsize());
+			
+			switch(elements.get(i).getType())
+			{
+				case LABEL: 
+					JLabel label = new JLabel(elements.get(i).getText());
+					label.setBounds(elements.get(i).getX(), elements.get(i).getY(),
+							elements.get(i).getWidth(),elements.get(i).getHeight());
+					label.setFont(f);
+					view.addLabel(label);
+					break;
+				case BUTTON:
+					JButton button = new JButton(elements.get(i).getText());
+					button.setText(elements.get(i).getText());
+					button.setBounds(elements.get(i).getX(), elements.get(i).getY(),
+							elements.get(i).getWidth(),elements.get(i).getHeight());
+					button.setFont(f);
+					button.setRolloverEnabled(false);
+					view.addButton(button);
+					break;
+				case TEXTFIELD:
+					JTextField textfield = new JTextField(elements.get(i).getText());
+					textfield.setBounds(elements.get(i).getX(), elements.get(i).getY(),
+							elements.get(i).getWidth(),elements.get(i).getHeight());
+					textfield.setFont(f);
+					view.addTextBox(textfield);
+					break;
+			}
+			
+			
+		}
 	}
 
 }
